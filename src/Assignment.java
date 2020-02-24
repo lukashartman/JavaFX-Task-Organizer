d487b9b09f699d6798e7ae87ec4a126bd2fc7c3f
Luke Hartma <lukehartman83@gmail.com>
2020-02-23 17:17:09 -0700

    Assignment and Course object

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,6 +1,16 @@
 public class Assignment {
 
-    public Assignment(){
+    private int dayDue, monthDue, yearDue;
+    private Course assignmentCourse;
+    private String assignmentName;
 
+    public Assignment(int day, int month, int year, Course assignmentCourse, String name){
+        this.assignmentCourse = assignmentCourse;
+        dayDue = day;
+        monthDue = month;
+        yearDue = year;
+        assignmentName = name;
     }
+
+
 }
---
+++ src/Course.java
@@ -0,0 +1,12 @@
+public class Course {
+
+    private String courseTitle, courseProfessor, courseLocation;
+
+    public Course(String title, String prof, String location){
+        courseTitle = title;
+        courseProfessor = prof;
+        courseLocation = location;
+    }
+
+
+}
